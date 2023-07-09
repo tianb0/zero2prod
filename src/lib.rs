a28305eb69e3800bc9e9dbb3a49748d21429dc3e
Tianbo <tianbo@hey.com>
2023-07-09 10:52:45 -0700

    port 0

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, dev::Server};
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
     })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    .listen(listener)?
+    .run();
+    Ok(server)
 }
