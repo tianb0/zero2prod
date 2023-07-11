f24b9f26ac2a25e091fb8022b0e0fc4640f36a3b
Tianbo <tianbo@hey.com>
2023-07-11 09:16:03 -0700

    update tests

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+use sqlx::{Connection, PgConnection, PgPool};
 use std::net::TcpListener;
-use zero2prod::{startup::run, configuration::get_configuration};
+use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // panic if we wan't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
+    // let connection = PgConnection::connect(
+    //     &configuration.database.connection_string()
+    // )
+    //     .await
+    //     .expect("Failed to connect to Postgres.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,24 @@
 use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpServer};
+use sqlx::{PgConnection, PgPool};
 
 use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(
+    listener: TcpListener,
+    // connection: PgConnection,
+    db_pool: PgPool,
+) -> Result<Server, std::io::Error> {
+
+    // wrap the connection in a smart pointer
+    let connection = web::Data::new(db_pool);
+
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
